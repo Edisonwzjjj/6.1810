af350789f6e1047b43da534ed65183b8c5a5deff
Edisonwzjjj <wangzijun2023@outlook.com>
2024-02-06 21:47:53 +1100

    lab1

---
+++ user/sleep.c
@@ -0,0 +1,24 @@
+//
+// Created by ZIJUN WANG on 6/2/2024.
+//
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int fd, i;
+
+    if(argc != 2){
+        fprintf(2, "Usage: sleep seconds\n");
+        exit(1);
+    }
+
+    int sec = atoi(argv[1]);
+    sleep(sec);
+
+    exit(0);
+}
+
