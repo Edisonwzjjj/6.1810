b11ca9cd434bfc80ed136af261e8a56bc31ace46
Edisonwzjjj <wangzijun2023@outlook.com>
2024-03-04 09:45:56 +1100

    cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,10 +9,16 @@
 #include "riscv.h"
 #include "defs.h"
 
+
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
+
+// the reference count of physical memory page
+int useReference[PHYSTOP/PGSIZE];
+struct spinlock ref_count_lock;
 
 struct run {
   struct run *next;
@@ -47,10 +53,19 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int temp; 
+
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
-
+  
+  acquire(&ref_count_lock);
+  useReference[(uint64) r / PGSIZE] -= 1;
+  temp = useReference[(uint64) r / PGSIZE]; 
+  release(&ref_count_lock); 
+  if (temp < 0) {
+    return;
+  }
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -72,8 +87,14 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    acquire(&ref_count_lock); 
+    useReference[(uint64) r / PGSIZE] = 1; 
+    release(&ref_count_lock); 
+
+  }
+
   release(&kmem.lock);
 
   if(r)
